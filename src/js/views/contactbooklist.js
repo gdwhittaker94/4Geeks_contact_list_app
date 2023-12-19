73fa950d8fba1b9e17c308ef2331a02b4a01f2a7
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-12-19 22:20:55 +0000

    Styling of main page

--- src/js/views/contactbooklist.js
+++ src/js/views/contactbooklist.js
@@ -6,7 +6,8 @@ import "../../styles/index.css";
 export const ContactBookList = () => {
     const { store, actions } = useContext(Context);
     const [contactBookName, setcontactBookName] = useState("");
-
+    const [width, setWidth] = useState(window.innerWidth);
+   
     // Fetch All Contact Books
     useEffect(() => {
         actions.fetchContactBooks();
@@ -18,35 +19,72 @@ export const ContactBookList = () => {
         store.inputResetToggle = false
     }, [store.inputResetToggle])
 
+    // Monitor Screen Size
+    // source: t.ly/dIZGD 
+    useEffect(() => {
+        const handleResize = () => {
+          setWidth(window.innerWidth);
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
+    // TO-DO: GET CHATGPT TO HELP ME WITH CHANGING ARROW DISPLAY -> currently blocks page display
+
     return (
-        <div className="container pb-4">
-            <div className="my-3 text-center d-flex flex-column align-items-center">
+        <div className="container pb-4 text-center">
+            <div className="header flex-column flex-sm-row">
+                <p>Create a new contact book {
+                    width >= 575? ⬇ : ➡️
+                    } </p>
                 <input
-                    placeholder="Type something to create"
+                    placeholder="Contact Book Name"
                     value={contactBookName}
                     onChange={(e) => setcontactBookName(e.target.value)}
-                    className="contactBookInput mb-2"
+                    className="contactBookInput"
                 />
                 {/* Conditional: what appears after input box */}
                 {contactBookName === "" ?
-                    <p>Create a new contacts book</p>
+                    null
                     :
                     <button onClick={() => actions.submitNewContactBook(contactBookName)}>
                         Create
                     </button>
                 }
-
             </div>
             {/* Agendas List */}
-            <ul className="contactBookList">
+            <ul className="contactBookList mx-4 mx-sm-0">
                 {store.allContactBooks.map((item, index) => {
                     return (
-                        <li key={index} className="bookListItem">
-                            <h2>{item}'s <br/> Contact Book</h2>
+                        <>
+                        <li key={index} className="contactBookList__item">
+                        <div className="contactBookList__binder">
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                            <div className="contactBookList__ring"></div>
+                        </div>
+                            <h2>{item}</h2>
+                            <h3>Contact Book</h3>
                             <Link to={`/listofcontact/${item}`} onClick={() => { actions.setCurrentBookName(item) }}>
-                                <button className="bookListItem__button">Enter</button>
+                                <button className="contactBookList__button">Enter</button>
                             </Link>
                         </li>
+                        </>
+                        
                     )
                 })}
             </ul>
