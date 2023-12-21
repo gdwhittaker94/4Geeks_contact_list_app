513690f8a3c465f902d38adcab87d036c745ceb7
David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-12-21 11:07:56 +0000

    returned basename

--- src/js/layout.js
+++ src/js/layout.js
@@ -7,6 +7,10 @@ import { ListOfContact } from "./views/listofcontact";
 import { AddContact } from "./views/addcontact";
 
 const Layout = () => {
+	// the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+	const basename = process.env.BASENAME || "";
+
 	return (
 		<div>
 			<BrowserRouter basename={basename}>
